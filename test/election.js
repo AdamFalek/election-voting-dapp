0810148e79e8a03fb41d5f62d8b587b6a01f8584
Adam Fałek <faleka@tt.com.pl>
2019-08-16 18:40:13 +0200

    voting app

---
+++ test/election.js
@@ -0,0 +1,27 @@
+const Election = artifacts.require("./Election.sol");
+
+contract("Election", function(accounts) {
+	it("initializes with two candidates", function() {
+		return Election.deployed().then(function(instance) {
+			return instance.candidatesCount();
+		}).then(function(count) {
+			assert.equal(count, 3);
+		})
+	})
+
+	it("allows a voter to cast a vote", function() {
+	    return Election.deployed().then(function(instance) {
+	    	electionInstance = instance;
+	      	candidateId = 1;
+	      	return electionInstance.vote(candidateId, { from: accounts[0] });
+	    }).then(function(receipt) {
+	      	return electionInstance.voters(accounts[0]);
+	    }).then(function(voted) {
+			assert(voted, "the voter was marked as voted");
+			return electionInstance.candidates(candidateId);
+	    }).then(function(candidate) {
+		    var voteCount = candidate[2];
+		    assert.equal(voteCount, 1, "increments the candidate's vote count");
+	    })
+	});
+})
